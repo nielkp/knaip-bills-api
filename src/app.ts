8b2d1c061cf67f8da34220c9ec4cceb607fd4d17
Daniel Knaip <danielknaip@gmail.com>
2025-07-14 11:37:50 -0300

    adicionando MongoDB via VPS

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import Fastify from 'fastify';
 import type { FastifyInstance } from 'fastify';
 import routes from './routes';
 
-const app: FastifyInstance = Fastify()
+const app: FastifyInstance = Fastify({
+  logger: true,
+})
 app.register(routes)
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv';
 import app from './app';
+dotenv.config();
 
-const PORT = 3001
+const PORT = Number(process.env.PORT);
 
 const startServer = async () => {
 
