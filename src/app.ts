d860e288cab85c76c92a2af89f7668a85644244e
Daniel Knaip <danielknaip@gmail.com>
2025-07-18 15:01:38 -0300

    adicionado log de IP's acessados externamente.

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,15 @@ import routes from './routes';
 
 const app: FastifyInstance = Fastify({
   logger: true,
+  trustProxy: true,
 });
-//adicioanr prefixo caso desejar, { prefix: '/api' }
+
+// Logar IP de todas as requisições
+app.addHook('onRequest', async (request, reply) => {
+  request.log.info({ ip: request.ip }, `Requisição recebida: ${request.method} ${request.url}`);
+});
+
+// Adicionar rotas (com prefixo se quiser, ex: { prefix: '/api' })
 app.register(routes);
 
-export default app
+export default app;
