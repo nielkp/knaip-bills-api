2bf1b97e1f4cc5c58bf6864c7125699147a7419c
Daniel Knaip <danielknaip@gmail.com>
2025-07-18 16:06:17 -0300

    adicionado icone

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import Fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
 import type { FastifyInstance } from 'fastify';
 import routes from './routes';
 
@@ -7,12 +9,18 @@ const app: FastifyInstance = Fastify({
   trustProxy: true,
 });
 
+// Servir arquivos estáticos como favicon.ico
+app.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+  prefix: '/', // Disponível como /favicon.ico
+});
+
 // Logar IP de todas as requisições
 app.addHook('onRequest', async (request, reply) => {
   request.log.info({ ip: request.ip }, `Requisição recebida: ${request.method} ${request.url}`);
 });
 
-// Adicionar rotas (com prefixo se quiser, ex: { prefix: '/api' })
+// Rotas principais
 app.register(routes);
 
 export default app;
