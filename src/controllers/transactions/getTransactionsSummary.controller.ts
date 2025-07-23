34c7ad5d9bcb7049110c7108f09139e84d4a30c0
Daniel Knaip <danielknaip@gmail.com>
2025-07-23 11:11:21 -0300

    alteração para arredondar o balance

--- src/controllers/transactions/getTransactionsSummary.controller.ts
+++ src/controllers/transactions/getTransactionsSummary.controller.ts
@@ -3,6 +3,9 @@ import type { GetTransactionsSummaryQuery } from "../../schemas/transaction.sche
 import dayjs from "dayjs";
 import utc from 'dayjs/plugin/utc'
 import prisma from "../../config/prisma";
+import type { CategorySummary } from "../../types/category.types";
+import { TransactionType } from "@prisma/client";
+import type { TransactionSummary } from "../../types/transaction.types";
 
 dayjs.extend(utc)
 
@@ -38,7 +41,42 @@ export const getTransactionsSummary = async (request: FastifyRequest<{ Querystri
       }
     });
 
-    reply.send(transactions);
+    let totalExpenses = 0;
+    let totalIncomes = 0;
+    const groupedExpenses = new Map<string, CategorySummary>();
+
+    for (const transaction of transactions) {
+
+      if (transaction.type === TransactionType.expense) {
+
+        const existing = groupedExpenses.get(transaction.categoryId) ?? {
+          categoryId: transaction.categoryId,
+          categoryName: transaction.Category.name,
+          categoryColor: transaction.Category.color,
+          amount: 0,
+          percentage: 0,
+        };
+
+        existing.amount += transaction.amount
+        groupedExpenses.set(transaction.categoryId, existing)
+
+        totalExpenses += transaction.amount
+      } else {
+        totalIncomes += transaction.amount
+      }
+    }
+
+    const summary: TransactionSummary = {
+      totalExpenses,
+      totalIncomes,
+      balance: Number((totalIncomes - totalExpenses).toFixed(2)),
+      expesesByCategory: Array.from(groupedExpenses.values()).map((entry) => ({
+        ...entry,
+        percentage: Number.parseFloat(((entry.amount / totalExpenses) * 100).toFixed(2)),
+      })).sort((a, b) => b.amount - a.amount),
+    }
+
+    reply.send(summary);
   } catch (err) {
     request.log.error("Erro ao trazer transações", err);
     reply.status(500).send({ error: "Erro do servidor" });
