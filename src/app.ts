17c336092bd5b1219c6b8557df3be1ab22a91da3
Daniel Knaip <danielknaip@gmail.com>
2025-07-29 21:46:52 -0300

    Melhorias no CORS!

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import type { FastifyInstance } from 'fastify';
 import routes from './routes';
 import cors from '@fastify/cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app: FastifyInstance = Fastify({
   logger: true,
@@ -20,8 +22,21 @@ app.register(fastifyStatic, {
 app.addHook('onRequest', async (request, reply) => {
   request.log.info({ ip: request.ip }, `Request recebido: ${request.method} ${request.url}`);
 });
+
 // Rota do FastifyCors
+const allowedOrigins = process.env.CORS_ORIGINS?.split(',').map(o => o.trim());
+
 app.register(cors, {
+  origin: (origin, callback) => {
+    console.log('Origem da requisição:', origin);
+
+    if (!origin || allowedOrigins?.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.error('❌ Origem bloqueada por CORS:', origin);
+      callback(new Error('Não perminito pelo CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
 });
 
